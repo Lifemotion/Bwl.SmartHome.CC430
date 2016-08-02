33d9a7d93aa7678e3b79f1bde2426fbc9e8dc790
Николай Гусев < >
2016-08-02 08:35:20 +0300

    (no message)

---
+++ RF.UsbAdapter/GUI/RF USB GUI v1.0/RF USB GUI v1.0/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace RF_USB_GUI_v1._0
+{
+    partial class bScan
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ports_list = new System.Windows.Forms.ComboBox();
+            this.bOpen = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ports_list
+            // 
+            this.ports_list.FormattingEnabled = true;
+            this.ports_list.Location = new System.Drawing.Point(13, 13);
+            this.ports_list.Name = "ports_list";
+            this.ports_list.Size = new System.Drawing.Size(87, 21);
+            this.ports_list.TabIndex = 0;
+            // 
+            // bOpen
+            // 
+            this.bOpen.Location = new System.Drawing.Point(106, 13);
+            this.bOpen.Name = "bOpen";
+            this.bOpen.Size = new System.Drawing.Size(75, 23);
+            this.bOpen.TabIndex = 1;
+            this.bOpen.Text = "open";
+            this.bOpen.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(585, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(585, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "send";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 134);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(585, 170);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 310);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(585, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "clear";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(187, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "scan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bScan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 345);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bOpen);
+            this.Controls.Add(this.ports_list);
+            this.Name = "bScan";
+            this.Text = "Rf GUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ports_list;
+        private System.Windows.Forms.Button bOpen;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RF.UsbAdapter/GUI/RF USB GUI v1.0/RF USB GUI v1.0/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RF_USB_GUI_v1._0
+{
+    public partial class bScan : Form
+    {
+        private SerialPort _serial = null;
+
+        public bScan()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String[] ports = SerialPort.GetPortNames();
+            foreach(String port in ports)
+            {
+                ports_list.Items.Add(port);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ports_list.Items.Clear();
+            String[] ports = SerialPort.GetPortNames();
+            foreach (String port in ports)
+            {
+                ports_list.Items.Add(port);
+            }
+        }
+    }
+}
